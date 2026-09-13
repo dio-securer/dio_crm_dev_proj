282d098461e8d72247efd24c288a3fc8f044a759
dio-securer <securer@dio.co.kr>
2026-09-13 10:40:52 +0900

    CRM-GL-001 G3: wire globalization context provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,15 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 import './i18n';
 import App from './App';
+import { GlobalizationProvider } from './market/globalization-context';
 
 const client = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
-      <BrowserRouter><App /></BrowserRouter>
+      <GlobalizationProvider>
+        <BrowserRouter><App /></BrowserRouter>
+      </GlobalizationProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
