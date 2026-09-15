e84a8379e9e8c7c626d452908d600e0675d88f32
dio-securer <securer@dio.co.kr>
2026-09-15 17:44:43 +0900

    feat: add HQ market template definition

---
+++ frontend/src/market/templates/HQ.ts
@@ -0,0 +1,9 @@
+import type { MarketTemplateDefinition } from '../types';
+
+export const HQ_TEMPLATE: MarketTemplateDefinition = {
+  code: 'HQ_TEMPLATE',
+  screenProfileCode: 'HQ_SCREEN_PROFILE',
+  fieldProfileCode: 'HQ_FIELD_PROFILE',
+  featureProfileCode: 'HQ_FEATURE_PROFILE',
+  integrationProfileCode: 'HQ_INTEGRATION_PROFILE'
+};
